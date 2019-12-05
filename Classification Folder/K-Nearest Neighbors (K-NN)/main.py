42fb930b8e9cc0ab036ca14be4c0070f8cc197e0
saeed <smohajer85@gmail.com>
2019-12-04 18:27:34 -0800

    New Changes

---
+++ Classification Folder/K-Nearest Neighbors (K-NN)/main.py
@@ -0,0 +1,85 @@
+"""
+Author : Saeed Mohajeryami, PhD
+"""
+
+
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+
+## take Age and Estimated Salary as features
+X = dataset.iloc[:,2:4].values
+y = dataset.iloc[:,-1].values
+
+### This part is reserved for when we add Gender to the model
+#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+#labelencoder = LabelEncoder()
+#X[:,0] = labelencoder.fit_transform(X[:,0])
+#
+#onehotencoder = OneHotEncoder(categorical_features=[0])
+#X = onehotencoder.fit_transform(X).toarray()
+
+### train/test split
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
+## feature scaling -> This part is necessary for visualization
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier(n_neighbors= 5)
+
+classifier.fit(X_train,y_train)
+
+y_pred = classifier.predict(X_test)
+
+
+### visualization of the training data
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+### visualization of the test data
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('KNN (test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+### evaluation
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+print(accuracy_score(y_test,y_pred))
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
