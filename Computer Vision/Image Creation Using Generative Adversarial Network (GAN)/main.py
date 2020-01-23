431f72a391ec5dfe54f76a8f2114c230f3c8d8f6
Saeed Mohajeryami,PhD <smohajer85@gmail.com>
2020-01-23 14:54:19 -0800

    GAN is added

--- Computer Vision/Image Creation Using Generative Adversarial Network (GAN)/main.py
+++ Computer Vision/Image Creation Using Generative Adversarial Network (GAN)/main.py
@@ -19,6 +19,7 @@ imageSize = 64 # We set the size of the generated images (64x64).
 # Creating the transformations
 transform = transforms.Compose([transforms.Scale(imageSize), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),]) # We create a list of transformations (scaling, tensor conversion, normalization) to apply to the input images.
 
+
 # Loading the dataset
 dataset = dset.CIFAR10(root = './data', download = True, transform = transform) # We download the training set in the ./data folder and we apply the previous transformations on each image.
 dataloader = torch.utils.data.DataLoader(dataset, batch_size = batchSize, shuffle = True, num_workers = 2) # We use dataLoader to get the images of the training set batch by batch.
